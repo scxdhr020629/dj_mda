44d9376359cd78ddda1034fd8bc1209be68aea72
鹤纸千 <1282026764@qq.com>
2025-02-24 10:10:29 +0800

    删除dj_map这个 app

--- dj_map/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class IPCount(models.Model):
-    ip = models.CharField(max_length=255)
-    location = models.CharField(max_length=255)
-    ISP = models.CharField(max_length=255)
--- dj_map/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from dj_map import models
-# Create your views here.
-
-def ip_list(request):
-    ip_queryset = models.
